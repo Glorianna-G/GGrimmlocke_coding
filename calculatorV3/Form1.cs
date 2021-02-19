63cc73d7e851d4c3c1afa193c133d72f172ff3b8
Glorianna-G <78559754+Glorianna-G@users.noreply.github.com>
2021-02-19 00:27:30 +0000

    Add files via upload

---
+++ calculatorV3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorV3
+{
+    public partial class Calculator1 : Form
+    {
+        public Calculator1()
+        {
+            InitializeComponent();
+        }
+
+        private void N1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void N0_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ calculatorV3/Form1v1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorV3
+{
+    public partial class Calculator1 : Form
+    {
+        public Calculator1()
+        {
+            InitializeComponent();
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "1";
+            }
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void n7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "7";
+            }
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "8";
+            }
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "9";
+            }
+        }
+
+        private void n0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "0";
+            }
+
+        }
+    }
+}
